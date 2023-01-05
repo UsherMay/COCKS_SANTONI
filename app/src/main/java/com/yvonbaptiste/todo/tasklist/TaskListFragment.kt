af0f0461f662e3259d3792933c92448f5c921a2c
Baton_ <baptiste.s@hotmail.fr>
2023-01-05 14:58:40 +0100

    ça marche

--- app/src/main/java/com/yvonbaptiste/todo/tasklist/TaskListFragment.kt
+++ app/src/main/java/com/yvonbaptiste/todo/tasklist/TaskListFragment.kt
@@ -4,10 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Adapter
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.yvonbaptiste.todo.R
+import java.util.*
+import kotlin.collections.List
 
 class TaskListFragment : Fragment()
 {
@@ -33,8 +35,19 @@ class TaskListFragment : Fragment()
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         val recyclerView = view.findViewById<RecyclerView>(R.id.recyclerView)
         recyclerView.adapter = adapter
+        val fabView = view.findViewById<FloatingActionButton>(R.id.add_task_fab);
         // super.onViewCreated(view, savedInstanceState)
 
+        fabView.setOnClickListener {
+            val newTask = Task(id = UUID.randomUUID().toString(), title = "Task ${taskList.size + 1}")
+            taskList = taskList + newTask
+            refreshAdapter()
+        }
+    }
+
+    fun refreshAdapter() {
+        adapter.currentList = taskList
+        adapter.notifyDataSetChanged()
     }
 
 
