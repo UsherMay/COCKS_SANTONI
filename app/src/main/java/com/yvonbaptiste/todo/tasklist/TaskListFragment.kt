ac0800bbab6b076f68bca5871837a9ddbf14b8c6
UsherMay <11yvon.00@gmail.com>
2023-01-15 22:17:31 +0100

    trying to implement swipeToDelete

--- app/src/main/java/com/yvonbaptiste/todo/tasklist/TaskListFragment.kt
+++ app/src/main/java/com/yvonbaptiste/todo/tasklist/TaskListFragment.kt
@@ -9,6 +9,8 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.RecyclerView
 import coil.load
 import com.yvonbaptiste.todo.R
 import com.yvonbaptiste.todo.data.Api
@@ -75,6 +77,20 @@ class TaskListFragment : Fragment()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         binding.recyclerView.adapter = adapter
+
+        /*
+        val swipeToDeleteCallback = object : SwipeToDeleteCallback(){
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                // val position = viewHolder.bindingAdapterPosition
+
+                viewModel.remove2(viewHolder.bindingAdapterPosition)
+            }
+        }
+
+        val itemTouchHelper = ItemTouchHelper(swipeToDeleteCallback)
+
+        itemTouchHelper.attachToRecyclerView(binding.recyclerView)
+         */
 
         lifecycleScope.launch { // on lance une coroutine car `collect` est `suspend`
             viewModel.tasksStateFlow.collect { newList ->
